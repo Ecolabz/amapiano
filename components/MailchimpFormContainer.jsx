58834caf796e9bf39ab766ff627d92c38f96312c
blockendsol <blockendsol@gmail.com>
2022-10-26 09:53:05 +0100

    ...testing

--- components/MailchimpFormContainer.jsx
+++ components/MailchimpFormContainer.jsx
@@ -50,7 +50,7 @@ const MailchimpFormContainer = props => {
   return (
     <div className="mc__form-container">
       <MailchimpSubscribe
-        url={postUrl}
+        url={mailchimp_url}
         render={({ subscribe, status, message }) => (
           <CustomForm status={status} message={message} onValidated={formData => subscribe(formData)} />
         )}
