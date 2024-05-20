d40a8bb0d4ce0d1929246d2f9f3d257a7b4fff58
Hector <adeniyi.olaitanhector@yahoo.com>
2024-05-20 23:13:34 +0100

    add clcikable support to button

--- components/general/Button.jsx
+++ components/general/Button.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import Link from 'next/link';
 
-function Button({ title, ...rest }) {
+function Button({ title, href, ...rest }) {
   return (
-    <button
-      className="flex items-center justify-center border-none bg-AP-blue-100 rounded-[10px] px-8 py-4 font-semibold font-montserat text-[16px] capitalize transition-all duration-500 hover:bg-AP-blue-200"
-      {...rest}
-    >
-      {title}
-    </button>
+    <>
+      {href ? (
+        <Link href={href} {...rest}>
+          <span className="flex items-center justify-center cursor-pointer border-none bg-AP-blue-100 rounded-[10px] px-8 py-4 font-semibold font-montserat text-[16px] capitalize transition-all duration-500 hover:bg-AP-blue-200">
+            {title}
+          </span>
+        </Link>
+      ) : (
+        <button
+          className="flex items-center justify-center cursor-pointer border-none bg-AP-blue-100 rounded-[10px] px-8 py-4 font-semibold font-montserat text-[16px] capitalize transition-all duration-500 hover:bg-AP-blue-200"
+          {...rest}
+        >
+          {title}
+        </button>
+      )}
+    </>
   );
 }
 
