1e8a121d0afa0025b136f32bee1c502d032fd428
Hector <adeniyi.olaitanhector@yahoo.com>
2024-05-20 23:16:47 +0100

    change text

--- sections/home/Landing.jsx
+++ sections/home/Landing.jsx
@@ -6,25 +6,34 @@ import NewsletterSubscribeForm from '../../components/general/NewsletterSubscrib
 
 function Landing() {
   return (
-    <div className="md:min-h-[90vh] min-h-[65vh] w-full h-full -no-repeat bg-cover flex flex-col items-center justify-center bg-center bg-homeHero py-7 md:py-0 px-2 relative">
-      <h1bg
+    <div className="md:min-h-[90vh] min-h-[65vh] w-full h-full -no-repeat bg-cover flex flex-col items-start justify-center bg-center bg-homeHero py-7 md:py-0 px-2 relative">
+      <h1
         data-aos="fade-right"
         data-aos-duration="1000"
-        className="font-[900] text-xl md:text-[48px] uppercase font-montserat md:leading-[58px] text-white text-center hidden md:block md:max-w-[998px] mx-auto"
+        className="font-[900] text-xl md:text-[48px] capitalize font-montserat md:leading-[58px] bg-white px-4 py-2 hidden md:block md:max-w-[998px] text-left"
       >
-        AMAPIANO LIFESTYLE IS HOSTING THE BIGGEST PARTY YOU’VE EVER SEEN.
-      </h1bg>
+        We Don't Host Parties
+      </h1>
+      <h1
+        data-aos="fade-right"
+        data-aos-duration="1000"
+        className="font-[900] text-xl md:text-[48px] capitalize font-montserat md:leading-[58px] bg-white px-4 py-2 hidden md:block md:max-w-[998px] text-left self-center mt-4"
+      >
+        We Are A Lifestyle Connoisseur
+      </h1>
       <h1 className="font-[900] block md:hidden text-center px-6 leading-[45px] text-3xl uppercase font-sansPro text-white">
         {' '}
         AMAPIANO LIFESTYLE <br /> IS HOSTING THE BIGGEST PARTY YOU’VE EVER SEEN.
       </h1>
-      <p
-        data-aos="fade-right"
-        data-aos-duration="1000"
-        className="tracking-[2%] md:py-3 text-white leading-[40px] text-[18px] md:text-[25px] font-sansPro"
-      >
-        Get ready for the event of the year !
-      </p>
+      <div className="self-center w-[53.5rem] mt-4">
+        <p
+          data-aos="fade-right"
+          data-aos-duration="1000"
+          className="hidden md:inline float-right tracking-[2%] md:py-3 self-end leading-[40px] text-[18px] md:text-[25px] bg-white px-4 py-2 font-sansPro"
+        >
+          Get ready for the event of the year !
+        </p>
+      </div>
       <div data-aos="fade-up" data-aos-duration="2000" className="w-full">
         <NewsletterSubscribeForm />
       </div>
