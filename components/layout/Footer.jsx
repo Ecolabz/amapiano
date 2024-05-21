e7a27dd250af3453064f84cfb5c8647cf630d41e
Hector <adeniyi.olaitanhector@yahoo.com>
2024-05-21 22:37:30 +0100

    add responsive style

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -7,18 +7,18 @@ import NavLink from '../general/NavLink';
 function Footer() {
   return (
     <div className="w-full flex flex-col text-center lg:items-center lg:max-w-[1000px] mx-auto  py-10 md:py-8 justify-center">
-      <div className="flex py-4  items-center max-w-[350px] mx-auto w-full justify-between">
+      <div className="flex py-4  items-center max-w-[350px] mx-auto w-full justify-evenly">
         {socialLinks.map(link => (
           <Icons key={link.id} icon={link.icon} href={link.href} />
         ))}
       </div>
-      <div className="grid grid-cols-2 md:flex gap-[4px] lg:justify-center py-6 lg:pt-0 lg:pb-4 px-4 text-left">
+      <div className="grid grid-cols-1 md:flex gap-[4px] justify-center py-6 lg:pt-0 lg:pb-4 px-4 text-left">
         {navLinks.map(link => (
           <NavLink
             key={link.id}
             href={link.href}
             text={link.text}
-            classNames="font-semibold py-2 lg:py-0 text-base md:text-[18px] hover:text-AP-blue-200"
+            classNames="font-semibold py-2 lg:py-0 text-center text-base md:text-[18px] hover:text-AP-blue-200"
             footer
           />
         ))}
