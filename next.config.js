e846281392f373f0d6e632ce345b1eb8651ee2d6
Deniyi <adeniyi.olaitanhector@yahoo.com>
2024-08-02 20:05:03 +0100

    add link redirects

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/tickets',
+        destination: 'https://senjaro.app/events/amapiano-lifestyle-summer-vibes-zanzibar-26-2024',
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
